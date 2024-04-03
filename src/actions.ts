5c32ec580841860efc7d0d946418abca9c8588ab
Shr4528 <Shr4528@gmail.com>
2024-04-03 18:00:55 -0500

    formData

--- src/actions.ts
+++ src/actions.ts
@@ -3,6 +3,11 @@
 import { sessionOptions, SessionData, defaultSession } from "@/lib";
 import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+let username = "user"
+let isPro = true
+
 
 export const getSession = async () => {
   const session = await getIronSession<SessionData>(cookies(), sessionOptions);
@@ -17,6 +22,19 @@ export const login = async (formData: FormData) => {
   const formUsername = formData.get('username') as string
   const formPassword = formData.get('password') as string
 
+  // check user in the db
+  // const user = await db.user.findUnique({username, password})
 
+  if(formUsername !== username){
+    return{error:"Wrong Credentials"}
+  }
+
+  session.userId ="1"
+  session.username = formUsername
+  session.isPro = isPro
+  session.isLoggedIn = true
+
+  await session.save()
+  redirect('/')
 };
 export const logout = async () => {};
